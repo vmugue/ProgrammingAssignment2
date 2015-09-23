b6d5d87fce1f0392aab3bff769a64632dd3f2a4f
vmugue <vmugue@gmail.com>
2015-09-23 22:13:15 +0200

    submit assignments

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,46 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL # when create new CachMatrix, old cache must be cleaned
+    set <- function(y) { 
+        x <<- y     # same when assign new matrix
+        inv <<- NULL
+    }
+    #return matrix (use x$get to see matrix)
+    get <- function() x
+    # set new invertion
+    setinv <- function(inv) inv <<- inv 
+    # get existed inversion
+    getinv <- function() inv
+    
+    # function returns object with functions which can be applied to x
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the invse of 'x'
+    
+    # get invertion from cache
+    inv <- x$getinv()
+    
+    # if cache doesn't exist message will be shown
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    # data - initial matrix, taken from cacheSolve object
+    data <- x$get()
+    #invertion calculation
+    inv <- solve(data, ...)
+    # write invertion in cache
+    x$setinv(inv)
+    #return invertion
+    inv
 }
+
+
